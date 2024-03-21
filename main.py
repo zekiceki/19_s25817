dfac554f8a05e2f362871bb4b0277256a22746d5
zekiceki <cemkasap2001@gmail.com>
2024-03-21 15:00:08 +0100

    Classes

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-def generate_squares(start, end):
-    return [x ** 2 for x in range(start, end + 1)]
+class SquareGenerator:
+    def generate_squares(self, start, end):
+        return [x ** 2 for x in range(start, end + 1)]
+
+
+square_gen = SquareGenerator()
 start = 1
 end = 10
-squares = generate_squares(start, end)
+squares = square_gen.generate_squares(start, end)
 print(f"List of squares of numbers from {start} to {end}:")
 print(squares)
