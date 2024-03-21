5767d84fccc04a9ccb3b53c26babdfcc31f582c4
zekiceki <cemkasap2001@gmail.com>
2024-03-21 15:54:51 +0100

    Inheritance

--- main.py
+++ main.py
@@ -2,22 +2,22 @@ import math
 
 from square_generator.square_generator import SquareGenerator
 
-square_gen = SquareGenerator()
+class CubicGenerator(SquareGenerator):
+    def generate_cubes(self, start, end):
 
+        return [x ** 3 for x in range(start, end + 1)]
+
+cubic_gen = CubicGenerator()
 
 start = 1
 end = 10
 
-try:
+cubes = cubic_gen.generate_cubes(start, end)
 
-    squares = square_gen.generate_squares(start, end)
+cube_roots = [math.pow(x, 1/3) for x in cubes]
 
-    square_roots = [math.sqrt(x) for x in squares]
+print(f"List of cubes of numbers from {start} to {end}:")
+print(cubes)
 
-    print(f"List of squares of numbers from {start} to {end}:")
-    print(squares)
-    print("Square roots of the numbers:")
-    print(square_roots)
-
-except ValueError as e:
-    print(f"Error: {e}")
+print("Cube roots of the numbers:")
+print(cube_roots)
