d00739c37ecc6555743ea32c25127b92a7bb5ad8
zekiceki <cemkasap2001@gmail.com>
2024-03-21 15:05:01 +0100

    Exceptions

--- main.py
+++ main.py
@@ -3,17 +3,27 @@ import math
 class SquareGenerator:
     def generate_squares(self, start, end):
 
+        if end < start:
+            raise ValueError("End of range cannot be less than the start.")
         return [x ** 2 for x in range(start, end + 1)]
+
 
 square_gen = SquareGenerator()
 
 start = 1
 end = 10
 
-squares = square_gen.generate_squares(start, end)
+try:
 
-square_roots = [math.sqrt(x) for x in squares]
-print(f"List of squares of numbers from {start} to {end}:")
-print(squares)
-print("Square roots of the numbers:")
-print(square_roots)
+    squares = square_gen.generate_squares(start, end)
+
+    square_roots = [math.sqrt(x) for x in squares]
+
+    print(f"List of squares of numbers from {start} to {end}:")
+    print(squares)
+
+    print("Square roots of the numbers:")
+    print(square_roots)
+
+except ValueError as e:
+    print(f"Error: {e}")
